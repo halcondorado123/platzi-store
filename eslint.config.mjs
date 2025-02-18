bd98e699fade75273ba1da364ebb358453a50ddf
halcondorado123 <halcondorado123@live.com>
2025-02-18 17:48:32 -0500

    Carga de archivo editorconfig y configuracion eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';      // Adicionar esta linea para evitar conflictos con Prettier
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -11,6 +12,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  eslintConfigPrettier, // 🔥 Desactiva reglas conflictivas con Prettier
   {
     languageOptions: {
       globals: {
@@ -25,11 +27,18 @@ export default tseslint.config(
       },
     },
   },
+  // Adicionar esta linea para evitar conflictos con Prettier
   {
     rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto', // 🔥 Evita problemas de saltos de línea
+        },
+      ],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
