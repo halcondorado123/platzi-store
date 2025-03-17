3654f9ee95ca0725260c0806022bbb720a276aac
Falcon Felipe <halcondorado123@live.com>
2025-03-17 16:43:39 -0500

    Actualizacion Update y DElete

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -25,30 +25,6 @@ export class ProductsController {
     return this.productsService.findAll();
   }
 
-  // @Get() // Manejo de parámetros de consulta con conversión
-  // getProductByQuery(
-  //   @Query('limit') limit?: string, // Recibe como string
-  //   @Query('offset') offset?: string, // Recibe como string
-  //   @Query('brand') brand?: string, // Opcional
-  // ) {
-  //   // Convertir los valores a números, asegurando valores por defecto si hay errores
-  //   const parsedLimit =
-  //     limit && !isNaN(Number(limit)) ? parseInt(limit, 10) : 100;
-  //   const parsedOffset =
-  //     offset && !isNaN(Number(offset)) ? parseInt(offset, 10) : 0;
-
-  //   return {
-  //     success: true, // Generalmente en APIs se usa "success" en lugar de "Success"
-  //     message: 'Products retrieved successfully',
-  //     data: {
-  //       limit: parsedLimit,
-  //       offset: parsedOffset,
-  //       brand: brand ?? 'not provided',
-  //     },
-  //   };
-  // }
-
-  // @Get('/products/filter') // Ya no sera necesario establecer products en la ruta
   @Get('filter') // Forma para definir el parametro
   getProductsFilter() {
     return {
@@ -60,69 +36,21 @@ export class ProductsController {
   @Get(':productId') // Forma para definir el parametro
   @HttpCode(HttpStatus.ACCEPTED) // Cambiar el código de estado de la respuesta
   getByIdParameters(@Param('productId') productId: string) {
-    // return {
-    //   success: true,
-    //   message: `Product ${productId} retrieved successfully`,
-    // };
     return this.productsService.findOne(+productId); // <-- Se agrega el signo + para convertir el string a número
   }
 
-  // @Get(':productId') // Forma para definir el parametro
-  // @HttpCode(HttpStatus.ACCEPTED) // Cambiar el código de estado de la respuesta
-  // getByIdParameters(@Param('productId') productId: string) {
-  //   return {
-  //     success: true,
-  //     message: `Product ${productId} retrieved successfully`,
-  //   };
-  // }
-
   @Post()
   create(@Body() payload: any) {
-    // return {
-    //   success: true,
-    //   message: 'Accion de crear, ejecutada exitosamente',
-    //   payload,
-    // };
     return this.productsService.create(payload);
   }
 
-  // @Post()
-  // create(@Body() payload: string) {
-  //   return {
-  //     success: true,
-  //     message: 'Accion de crear, ejecutada exitosamente',
-  //     payload,
-  //   };
-  // }
-
-  // Patch según RESTful es para actualizar parcialmente un recurso
   @Put(':id')
-  update(@Param('id') id: string, @Body() payload: string) {
+  update(@Param('id') id: string, @Body() payload: any) {
     return this.productsService.update(+id, payload);
   }
 
-  // @Put(':id')
-  // update(@Param('id') id: number, @Body() payload: string) {
-  //   return {
-  //     success: true,
-  //     message: '¡Acción de actualizar, ejecutada exitosamente!',
-  //     payload,
-  //   };
-  // }
-
   @Delete(':id')
-  delete(@Param('id') id: number) {
-    return {
-      success: true,
-      message: `¡Acción de eliminar, ejecutada para el id ${id}!`,
-    };
+  delete(@Param('id') id: string) {
+    return this.productsService.remove(+id);
   }
-
-  // @Delete(':id')
-  // delete(@Param('id') id: number) {
-  //   return {
-  //     success: true,
-  //     message: `¡Acción de eliminar, ejecutada para el id ${id}!`,
-  //   };
-  // }
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from '../entities/product.entity';
 
 // Se busca administrar en memoria los productos, aqui mismo se peude cargar los servicios en bases de datos
@@ -22,7 +22,11 @@ export class ProductsService {
   }
 
   findOne(id: number) {
-    return this.products.find((item) => item.id === id);
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return { message: 'Product is found', product };
   }
 
   create(payload: any) {
@@ -48,15 +52,12 @@ export class ProductsService {
     return null;
   }
 
-  delete(id: number) {
-    const productFound = this.products.findIndex((item) => item.id === id);
-    let message = '';
-    if (productFound > 0) {
-      this.products.splice(productFound, 1);
-      message = 'Product deleted';
-    } else {
-      message = 'Product not found';
+  remove(id: number) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Product #${id} not found`);
     }
-    return message;
+    this.products.splice(index, 1);
+    return true;
   }
 }
